0fda8420a047260fe72ad6f53541c74298c2a1fd
NaveenKumarSubramanian <106928795+NaveenKumarSubramanian@users.noreply.github.com>
2022-06-06 00:52:04 +0530

    Add files via upload
    commit changes

---
+++ Jenkins.java
@@ -0,0 +1,8 @@
+public class Jenkins {
+
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+    System.out.println("Hello Agni");
+ System.out.println("Hello Agniriya");
+ System.out.println("Hello riya");
+}}
